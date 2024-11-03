c8aef075b9f26900699a65186d91e000e500a175
Gargenziano03 <gargenziano03@gmail.com>
2024-11-03 15:27:43 +0100

    add folder Data

---
+++ myapp/Data/posts.js
@@ -0,0 +1,34 @@
+const posts = [
+    {
+        title: "La bellezza della natura",
+        content: "Esplora i meravigliosi paesaggi naturali che il nostro pianeta ha da offrire.",
+        image: "natura.jpg",
+        tags: ["natura", "paesaggi", "avventura"]
+    },
+    {
+        title: "Cucina Italiana: Un viaggio tra i sapori",
+        content: "Scopri le ricette tradizionali italiane e impara a cucinarle a casa.",
+        image: "cucina_italiana.jpg",
+        tags: ["cibo", "ricette", "italiano"]
+    },
+    {
+        title: "Fitness: La tua guida per un corpo sano",
+        content: "Segui il nostro programma di allenamento per mantenerti in forma e sano.",
+        image: "fitness.jpg",
+        tags: ["fitness", "salute", "allenamento"]
+    },
+    {
+        title: "Tecnologia: Le ultime novità",
+        content: "Rimani aggiornato sulle ultime innovazioni e tendenze tecnologiche.",
+        image: "tecnologia.jpg",
+        tags: ["tecnologia", "novità", "innovazione"]
+    },
+    {
+        title: "Viaggi: Destinazioni da sogno",
+        content: "Scopri i luoghi più affascinanti da visitare in tutto il mondo.",
+        image: "viaggi.jpg",
+        tags: ["viaggi", "avventura", "esplorazione"]
+    }
+];
+
+module.exports = posts
